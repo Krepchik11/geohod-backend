e0b4918765dde3672df9920089df340e85a1fe3e
Alexey Naborshchikov <to.magus@gmail.com>
2024-11-18 19:44:32 +0100

    initial commit

---
+++ src/test/java/me/geohod/geohodbackend/GeohodBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package me.geohod.geohodbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeohodBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
