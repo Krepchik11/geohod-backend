80cae785b094e6ab2a640c7142cbe7664279f787
Alexey Naborshchikov <to.magus@gmail.com>
2024-12-18 17:08:54 +0100

    request & response, cors logging added

--- src/main/java/me/geohod/geohodbackend/configuration/ExceptionHandlerAdvice.java
+++ src/main/java/me/geohod/geohodbackend/configuration/ExceptionHandlerAdvice.java
@@ -1,23 +1,26 @@
 package me.geohod.geohodbackend.configuration;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @ControllerAdvice
+@Slf4j
 public class ExceptionHandlerAdvice {
-    @ExceptionHandler(IllegalArgumentException.class)
-    public ResponseEntity<String> handleException(IllegalArgumentException e) {
-        return ResponseEntity
-                .status(HttpStatus.BAD_REQUEST)
-                .body(e.getMessage());
-    }
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<Object> handleException(Exception ex, WebRequest request) {
+        Map<String, Object> errorDetails = new HashMap<>();
+        errorDetails.put("error", ex.getMessage());
+        errorDetails.put("path", request.getDescription(false));
 
-    @ExceptionHandler(IllegalStateException.class)
-    public ResponseEntity<String> handleException(IllegalStateException e) {
-        return ResponseEntity
-                .status(HttpStatus.BAD_REQUEST)
-                .body(e.getMessage());
+        log.error("System error occurred: {}", errorDetails, ex);
+
+        return new ResponseEntity<>(errorDetails, HttpStatus.INTERNAL_SERVER_ERROR);
     }
 }
