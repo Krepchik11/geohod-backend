62bedb4dd20d2346ea04816d499eef176953dca4
Alexey Naborshchikov <to.magus@gmail.com>
2025-01-29 12:40:44 +0100

    only up to 30 minutes old notifications now processing

--- src/main/java/me/geohod/geohodbackend/data/model/repository/TelegramOutboxMessageRepository.java
+++ src/main/java/me/geohod/geohodbackend/data/model/repository/TelegramOutboxMessageRepository.java
@@ -1,5 +1,6 @@
 package me.geohod.geohodbackend.data.model.repository;
 
+import java.time.Instant;
 import java.util.UUID;
 
 import org.springframework.data.domain.Page;
@@ -9,5 +10,5 @@ import org.springframework.data.repository.CrudRepository;
 import me.geohod.geohodbackend.data.model.TelegramOutboxMessage;
 
 public interface TelegramOutboxMessageRepository extends CrudRepository<TelegramOutboxMessage, UUID> {
-    Page<TelegramOutboxMessage> findAllByProcessedIsFalse(Pageable pageable);
+    Page<TelegramOutboxMessage> findAllByProcessedIsFalseAndCreatedAtAfter(Instant after, Pageable pageable);
 }
