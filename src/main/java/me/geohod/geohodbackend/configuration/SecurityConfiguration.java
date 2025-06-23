bb4ca3ab6f0f1f70e27d20baf53127fda3fd8097
Alexey Naborshchikov <to.magus@gmail.com>
2025-06-23 16:37:42 +0200

    Revert "config"
    Issuficient cors filter and unused logging dev mode
    
    This reverts commit d02f1bec8a006e0a66e5ced4077d87b291759716.

--- src/main/java/me/geohod/geohodbackend/configuration/SecurityConfiguration.java
+++ src/main/java/me/geohod/geohodbackend/configuration/SecurityConfiguration.java
@@ -1,11 +1,9 @@
 package me.geohod.geohodbackend.configuration;
 
-import me.geohod.geohodbackend.configuration.properties.SecurityProperties;
 import me.geohod.geohodbackend.security.filter.TelegramInitDataAuthenticationFilter;
 import me.geohod.geohodbackend.security.provider.TelegramTokenAuthenticationProvider;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.env.Environment;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.ProviderManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
@@ -15,31 +13,15 @@ import org.springframework.security.config.annotation.web.configurers.AbstractHt
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.filter.CorsFilter;
-
-import java.util.Arrays;
-import java.util.Collections;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfiguration {
     private final ProviderManager providerManager;
     private final TelegramTokenAuthenticationProvider telegramTokenAuthenticationProvider;
-    private final CorsFilter corsFilter;
-    private final Environment environment;
-    private final SecurityProperties securityProperties;
 
-    public SecurityConfiguration(TelegramTokenAuthenticationProvider telegramTokenAuthenticationProvider,
-                                CorsFilter corsFilter,
-                                Environment environment,
-                                SecurityProperties securityProperties) {
+    public SecurityConfiguration(TelegramTokenAuthenticationProvider telegramTokenAuthenticationProvider) {
         this.telegramTokenAuthenticationProvider = telegramTokenAuthenticationProvider;
-        this.corsFilter = corsFilter;
-        this.environment = environment;
-        this.securityProperties = securityProperties;
         this.providerManager = new ProviderManager(
                 telegramTokenAuthenticationProvider
         );
@@ -54,10 +36,8 @@ public class SecurityConfiguration {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http
                 .csrf(AbstractHttpConfigurer::disable)
-                .cors(cors -> cors.configurationSource(corsConfigurationSource()))
                 .authorizeHttpRequests(authorize -> authorize.anyRequest().fullyAuthenticated())
                 .authenticationProvider(telegramTokenAuthenticationProvider)
-                .addFilterBefore(corsFilter, TelegramInitDataAuthenticationFilter.class)
                 .addFilterBefore(loggingFilter(), UsernamePasswordAuthenticationFilter.class)
                 .addFilterBefore(tgInitDataAuthFilter(), UsernamePasswordAuthenticationFilter.class)
                 .sessionManagement(sessionConfigurer -> sessionConfigurer
@@ -65,20 +45,8 @@ public class SecurityConfiguration {
                 .build();
     }
 
-    @Bean
-    public CorsConfigurationSource corsConfigurationSource() {
-        CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Collections.singletonList("*"));
-        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
-        configuration.setAllowedHeaders(Arrays.asList("authorization", "content-type", "x-auth-token"));
-        configuration.setExposedHeaders(Collections.singletonList("x-auth-token"));
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", configuration);
-        return source;
-    }
-
     private TelegramInitDataAuthenticationFilter tgInitDataAuthFilter() {
-        return new TelegramInitDataAuthenticationFilter(providerManager, environment, securityProperties);
+        return new TelegramInitDataAuthenticationFilter(providerManager);
     }
 
     private LoggingFilter loggingFilter() {
