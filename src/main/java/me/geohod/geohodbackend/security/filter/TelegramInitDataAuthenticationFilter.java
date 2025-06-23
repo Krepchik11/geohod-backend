371e396e3e13c9795294d816aeade5d995f5a491
Alexey Naborshchikov <to.magus@gmail.com>
2025-06-23 16:37:31 +0200

    Revert "annotation"
    Incorrect filter order
    
    This reverts commit 8084f7021b471a770579fb90433dd4c9e8d3a177.

--- src/main/java/me/geohod/geohodbackend/security/filter/TelegramInitDataAuthenticationFilter.java
+++ src/main/java/me/geohod/geohodbackend/security/filter/TelegramInitDataAuthenticationFilter.java
@@ -16,8 +16,6 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.filter.OncePerRequestFilter;
-import org.springframework.core.annotation.Order;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import java.io.IOException;
 import java.util.Arrays;
@@ -25,7 +23,6 @@ import java.util.UUID;
 
 @RequiredArgsConstructor
 @Slf4j
-@Order(UsernamePasswordAuthenticationFilter.BASIC_AUTH_FILTER_ORDER - 1)
 public class TelegramInitDataAuthenticationFilter extends OncePerRequestFilter {
     private final AuthenticationManager authenticationManager;
     private final Environment environment;
