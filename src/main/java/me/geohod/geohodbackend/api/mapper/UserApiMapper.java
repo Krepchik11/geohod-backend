9ada50287fe1b9293b016b4538f3b679436924b8
Alexey Naborshchikov <to.magus@gmail.com>
2025-08-05 18:06:24 +0200

    cleanup warnings

--- src/main/java/me/geohod/geohodbackend/api/mapper/UserApiMapper.java
+++ src/main/java/me/geohod/geohodbackend/api/mapper/UserApiMapper.java
@@ -1,14 +1,21 @@
 package me.geohod.geohodbackend.api.mapper;
 
+import org.mapstruct.Mapper;
+
 import me.geohod.geohodbackend.api.dto.response.EventParticipantDetails;
 import me.geohod.geohodbackend.api.dto.response.UserResponse;
 import me.geohod.geohodbackend.data.dto.EventParticipantProjection;
 import me.geohod.geohodbackend.data.dto.UserDto;
 import me.geohod.geohodbackend.mapper.GlobalMapperConfig;
-import org.mapstruct.Mapper;
 
 @Mapper(config = GlobalMapperConfig.class)
 public interface UserApiMapper {
     EventParticipantDetails map(EventParticipantProjection dto);
+    
     UserResponse map(UserDto user);
+    
+    default java.util.UUID map(String id) {
+        return id != null ? java.util.UUID.fromString(id) : null;
+    }
 }
+
