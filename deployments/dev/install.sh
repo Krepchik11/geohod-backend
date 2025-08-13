e72a5dd33d4147ca543f40809deaddb3b7770888
Alexey Naborshchikov <to.magus@gmail.com>
2025-08-13 11:19:57 +0200

    fix service naming

--- deployments/dev/install.sh
+++ deployments/dev/install.sh
@@ -2,7 +2,7 @@
 set -euo pipefail
 
 STAGING_DIR="${HOME}/geohod-backend-dev"
-SERVICE_NAME="geohod-dev"
+SERVICE_NAME="geohod-backend-dev"
 IMAGE_TARBALL="${STAGING_DIR}/geohod-backend-image.tar.gz"
 SERVICE_DIR="${HOME}/.config/systemd/user"
 
