ccaabfa61be621c1524c35eed43e0c3ff48358c2
Aleksey Naborshchikov <to.magus@gmail.com>
2025-10-03 11:19:22 +0200

    api contract: add user settings update endpoints for max participants, payment gateway url, show become organizer (#76)

--- src/main/java/me/geohod/geohodbackend/user_settings/mapper/UserSettingsMapper.java
+++ src/main/java/me/geohod/geohodbackend/user_settings/mapper/UserSettingsMapper.java
@@ -1,14 +1,18 @@
 package me.geohod.geohodbackend.user_settings.mapper;
 
-import me.geohod.geohodbackend.user_settings.data.model.UserSettings;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+import me.geohod.geohodbackend.mapper.GlobalMapperConfig;
 import me.geohod.geohodbackend.user_settings.api.dto.UserSettingsRequest;
 import me.geohod.geohodbackend.user_settings.api.dto.UserSettingsResponse;
+import me.geohod.geohodbackend.user_settings.data.model.UserSettings;
 import me.geohod.geohodbackend.user_settings.service.dto.UserSettingsDto;
-import org.mapstruct.Mapper;
-import me.geohod.geohodbackend.mapper.GlobalMapperConfig;
 
 @Mapper(config = GlobalMapperConfig.class)
 public interface UserSettingsMapper {
+    @Mapping(target = "paymentGatewayUrl", constant = "")
+    @Mapping(target = "showBecomeOrganizer", constant = "true")
     UserSettingsResponse toResponse(UserSettingsDto dto);
     UserSettingsDto toDto(UserSettings userSettings);
     UserSettings toEntity(UserSettingsDto dto);
