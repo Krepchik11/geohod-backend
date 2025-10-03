51257ddf66e6f2ccf68e87af2492fcfd328af9df
Alexey Naborshchikov <to.magus@gmail.com>
2025-10-03 11:16:56 +0200

    api contract: add user settings update endpoints for max participants, payment gateway url, show become organizer

--- src/main/java/me/geohod/geohodbackend/user_settings/api/dto/UserSettingsRequest.java
+++ src/main/java/me/geohod/geohodbackend/user_settings/api/dto/UserSettingsRequest.java
@@ -4,9 +4,12 @@ package me.geohod.geohodbackend.user_settings.api.dto;
  * DTO for incoming PUT requests to update user settings.
  */
 public record UserSettingsRequest(
+    @Deprecated
     String defaultDonationAmount,
-    Integer defaultMaxParticipants
+    Integer defaultMaxParticipants,
+    String paymentGatewayUrl,
+    boolean showBecomeOrganizer
 ) {
     public UserSettingsRequest {
     }
-} 
+}
