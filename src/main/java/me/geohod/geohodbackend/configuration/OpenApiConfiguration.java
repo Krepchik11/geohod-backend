e06fd5a0efd767fc3ff46b87b79c021a3b3fcb1f
Alexey Naborshchikov <to.magus@gmail.com>
2025-10-21 17:29:49 +0200

    openapi ver

--- src/main/java/me/geohod/geohodbackend/configuration/OpenApiConfiguration.java
+++ src/main/java/me/geohod/geohodbackend/configuration/OpenApiConfiguration.java
@@ -1,5 +1,10 @@
 package me.geohod.geohodbackend.configuration;
 
+import java.util.List;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
@@ -7,10 +12,6 @@ import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
 import io.swagger.v3.oas.models.security.SecurityScheme.In;
 import io.swagger.v3.oas.models.servers.Server;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-
-import java.util.List;
 
 @Configuration
 public class OpenApiConfiguration {
@@ -20,7 +21,8 @@ public class OpenApiConfiguration {
         return new OpenAPI()
                 .info(new Info()
                         .title("Geohod Backend API")
-                        .description("Backend API for Geohod event management system"))
+                        .description("Backend API for Geohod event management system")
+                        .version("2.0.0"))
                 .servers(List.of(
                         new Server().url("http://localhost:8080").description("Local development server")
                 ))
