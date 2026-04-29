708c3327b7782800d502fdccece7289335ca4c28
Aleksey Naborshchikov <to.magus@gmail.com>
2026-04-29 08:12:41 +0200

    added `/api/v2/users/me` endpoint (#132)

--- src/main/java/me/geohod/geohodbackend/api/dto/response/UserDetailsResponse.java
+++ src/main/java/me/geohod/geohodbackend/api/dto/response/UserDetailsResponse.java
@@ -1,6 +1,9 @@
 package me.geohod.geohodbackend.api.dto.response;
 
+import java.util.UUID;
+
 public record UserDetailsResponse(
+    UUID id,
     String name,
     String username,
     String imageUrl
