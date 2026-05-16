b613b09b4daab9093cfde1f90ed61aa791642787
Alexey Naborshchikov <to.magus@gmail.com>
2026-05-16 16:08:45 +0200

    fix: disable scheduling in tests to prevent post-shutdown DB errors (#138)

---
+++ src/main/java/me/geohod/geohodbackend/configuration/SchedulingConfiguration.java
@@ -0,0 +1,11 @@
+package me.geohod.geohodbackend.configuration;
+
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@Configuration
+@EnableScheduling
+@ConditionalOnProperty(name = "geohod.scheduling.enabled", havingValue = "true", matchIfMissing = true)
+public class SchedulingConfiguration {
+}
